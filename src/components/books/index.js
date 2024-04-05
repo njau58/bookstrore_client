ce88174875a01a3cdc321675b11f13c6db914e2d
njauprince@gmail.com <njauprince@gmail.com>
2024-04-05 21:46:10 +0300

    production mpesaexpress

--- src/components/books/index.js
+++ src/components/books/index.js
@@ -1,60 +1,68 @@
-import { think_grow_rich,dark, now, creative_thinking, mindset,habits,smart } from "../../assets";
+import {
+  think_grow_rich,
+  dark,
+  now,
+  creative_thinking,
+  mindset,
+  habits,
+  smart,
+} from "../../assets";
 
 const book_store = [
   {
-    book_id:'08b3272c-5d44-11ee-8c99-0242ac120002' ,
+    book_id: "08b3272c-5d44-11ee-8c99-0242ac120002",
     book_title: "Think and Grow Rich",
-    price: 1,
+    price: 20,
     book_img: think_grow_rich,
     execerpt:
       "Think and Grow Rich: The Landmark Bestseller Now Revised and Updated for the 21st Century (Think and Grow Rich Series)",
   },
 
   {
-    book_id: '08b32a1a-5d44-11ee-8c99-0242ac120002',
+    book_id: "08b32a1a-5d44-11ee-8c99-0242ac120002",
     book_title: "The Art of Creative Thinking",
-    price: 1,
+    price: 20,
     book_img: creative_thinking,
     execerpt:
       "THE ART OF CREATIVE THINKING reveals how we can transform our businesses, our society and ourselves through a deeper understanding of human creativity.",
   },
 
   {
-    book_id:'08b32b82-5d44-11ee-8c99-0242ac120002' ,
+    book_id: "08b32b82-5d44-11ee-8c99-0242ac120002",
     book_title: "Mindset",
-    price: 1,
+    price: 20,
     book_img: mindset,
     execerpt:
       "The New Psychology of Success: by Carol S. Dweck is a thought-provoking and insightful book that challenges our beliefs about talent, intelligence...",
   },
   {
-    book_id: '08b32e0c-5d44-11ee-8c99-0242ac120002',
+    book_id: "08b32e0c-5d44-11ee-8c99-0242ac120002",
     book_title: "The Seven Habits of Highly Effective People",
-    price: 1,
+    price: 20,
     book_img: habits,
     execerpt:
       "The 7 Habits of Highly Effective People is the proven team effectiveness operating system. Participants increase team engagement, morale, and collaboration",
   },
   {
-    book_id:'08b32f42-5d44-11ee-8c99-0242ac120002' ,
+    book_id: "08b32f42-5d44-11ee-8c99-0242ac120002",
     book_title: "The Power of Now",
-    price: 1,
+    price: 20,
     book_img: now,
     execerpt:
       "Much more than simple principles and platitudes, the book takes readers on an inspiring spiritual journey to find their true and deepest self and reach the ...",
   },
   {
-    book_id: '08b32f42-5d44-11ee-8c99-0242ac120002',
+    book_id: "08b32f42-5d44-11ee-8c99-0242ac120002",
     book_title: "Dark Psychology Secret",
-    price: 1,
+    price: 20,
     book_img: dark,
     execerpt:
       " An informative short book that describes ways in which one can detect and avoid manipulation, mind control and deception techniques when someone ...",
   },
   {
-    book_id: '08b32cd6-5d44-11ee-8c99-0242ac120002',
+    book_id: "08b32cd6-5d44-11ee-8c99-0242ac120002",
     book_title: "Smart Thinking",
-    price: 1,
+    price: 20,
     book_img: smart,
     execerpt:
       "Smart Thinking: Skills for Critical Understanding and Writing is a practical, step-by-step guide to improving skills in analysis, critical thinking, and the effective communication of arguments and",
